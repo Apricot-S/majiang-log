2e1268e1875e9f8ad3e271a7f398bc7c6a7e0e4d
Apricot-S <mahjong_apricot@outlook.jp>
2024-07-03 21:36:41 +0000

    feat: Allow mode to be specified at request time instead of at startup

--- src/lib/convertLog.ts
+++ src/lib/convertLog.ts
@@ -114,7 +114,7 @@ export const MODE = {
 
 export type Mode = (typeof MODE)[keyof typeof MODE];
 
-export const isMode = (mode: string): mode is Mode => {
+const isMode = (mode: string): mode is Mode => {
   const modes: string[] = Object.values(MODE);
   return modes.includes(mode);
 };
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@
 import { parseArgs } from 'node:util';
 import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts';
 import Fastify, { FastifyInstance } from 'fastify';
-import { convertLog, isMode, Mode, MODE } from './lib/convertLog.js';
+import { convertLog, MODE } from './lib/convertLog.js';
 import { MAJIANG_LOG_SCHEMA } from './lib/schema.js';
 
 const options = {
@@ -19,18 +19,11 @@ const options = {
     multiple: false,
     default: '/majiang-log/',
   },
-  mode: {
-    type: 'string',
-    short: 'm',
-    multiple: false,
-    default: MODE.Log,
-  },
 } as const;
 
 type Options = {
   port: number;
   baseurl: string;
-  mode: Mode;
 };
 
 const getOptions = (): Options => {
@@ -47,25 +40,31 @@ const getOptions = (): Options => {
       .replace(/^(?!\/.*)/, '/$&')
       .replace(/\/$/, '') + '/';
 
-  const mode = parsedArgs.values.mode!;
-  if (!isMode(mode)) {
-    throw new Error(`Invalid mode: ${mode}`);
-  }
-
-  return { port, baseurl, mode };
+  return { port, baseurl };
 };
 
-const createServer = (baseurl: string, mode: Mode): FastifyInstance => {
+const createServer = (baseurl: string): FastifyInstance => {
   const server = Fastify({
     logger: true,
   }).withTypeProvider<JsonSchemaToTsProvider>();
 
   const schema = { body: MAJIANG_LOG_SCHEMA };
-  server.post(baseurl, { schema }, async (request, reply) => {
+
+  server.post(`${baseurl}${MODE.Log}`, { schema }, async (request, reply) => {
     const data = request.body;
-    const output = convertLog(data, mode);
+    const output = convertLog(data, MODE.Log);
     reply.header('Content-Type', 'application/json').code(200).send(output);
   });
+
+  server.post(
+    `${baseurl}${MODE.Viewer}`,
+    { schema },
+    async (request, reply) => {
+      const data = request.body;
+      const output = convertLog(data, MODE.Viewer);
+      reply.header('Content-Type', 'application/json').code(200).send(output);
+    },
+  );
 
   return server;
 };
@@ -87,7 +86,7 @@ const startServer = (server: FastifyInstance, port: number) => {
 
 const main = () => {
   const options = getOptions();
-  const server = createServer(options.baseurl, options.mode);
+  const server = createServer(options.baseurl);
   startServer(server, options.port);
 };
 
