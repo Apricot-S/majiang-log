74abf270bc80f535cf6f2fe11e4f9d6ff5c59592
Apricot-S <mahjong_apricot@outlook.jp>
2024-07-08 15:22:11 +0000

    fix: Throw an error if the number of properties in action is other than 1

--- src/lib/convertLog.ts
+++ src/lib/convertLog.ts
@@ -101,6 +101,11 @@ const convertRound = (
 
   for (const action of round) {
     const keys = Object.keys(action);
+    if (keys.length !== 1) {
+      throw keys.length > 1
+        ? new Error(`An action has multiple properties.: ${keys.length}`)
+        : new Error('An action has no property.');
+    }
     const key = keys[0] as keyof MajiangAction;
     const handler = handlers[key];
     if (handler) {
