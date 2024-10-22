b028436c9ab94f4ce447298f45a11ba1734850a1
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-22 23:21:45 +0000

    test: Add test cases

--- src/__tests__/lib/rotate.test.ts
+++ src/__tests__/lib/rotate.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from 'vitest';
-import { rotatePlayer } from '../../lib/rotate.js';
+import { rotatePlayer, rotateOrder } from '../../lib/rotate.js';
 
 test('player: 0, jushu: 0 -> position at the start of the game: 0', () => {
   expect(rotatePlayer(0, 0)).toBe(0);
@@ -31,4 +31,20 @@ test('player: 0, jushu: 3 -> position at the start of the game: 3', () => {
 
 test('player: 3, jushu: 3 -> position at the start of the game: 2', () => {
   expect(rotatePlayer(3, 3)).toBe(2);
+});
+
+test('[0, 1, 2, 3], jushu: 0 -> [0, 1, 2, 3]', () => {
+  expect(rotateOrder([0, 1, 2, 3], 0)).toEqual([0, 1, 2, 3]);
+});
+
+test('[0, 1, 2, 3], jushu: 1 -> [3, 0, 1, 2]', () => {
+  expect(rotateOrder([0, 1, 2, 3], 1)).toEqual([3, 0, 1, 2]);
+});
+
+test('[0, 1, 2, 3], jushu: 2 -> [2, 3, 0, 1]', () => {
+  expect(rotateOrder([0, 1, 2, 3], 2)).toEqual([2, 3, 0, 1]);
+});
+
+test('[0, 1, 2, 3], jushu: 3 -> [1, 2, 3, 0]', () => {
+  expect(rotateOrder([0, 1, 2, 3], 3)).toEqual([1, 2, 3, 0]);
 });
