7e18e67bd7befd4d767f51a3df836914b870ae45
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-22 14:06:12 +0000

    test: Add test cases

--- src/__tests__/lib/rotate.test.ts
+++ src/__tests__/lib/rotate.test.ts
@@ -12,3 +12,23 @@ test('player: 3, jushu: 0 -> position at the start of the game: 3', () => {
 test('player: 0, jushu: 1 -> position at the start of the game: 1', () => {
   expect(rotatePlayer(0, 1)).toBe(1);
 });
+
+test('player: 3, jushu: 1 -> position at the start of the game: 0', () => {
+  expect(rotatePlayer(3, 1)).toBe(0);
+});
+
+test('player: 0, jushu: 2 -> position at the start of the game: 2', () => {
+  expect(rotatePlayer(0, 2)).toBe(2);
+});
+
+test('player: 3, jushu: 2 -> position at the start of the game: 1', () => {
+  expect(rotatePlayer(3, 2)).toBe(1);
+});
+
+test('player: 0, jushu: 3 -> position at the start of the game: 3', () => {
+  expect(rotatePlayer(0, 3)).toBe(3);
+});
+
+test('player: 3, jushu: 3 -> position at the start of the game: 2', () => {
+  expect(rotatePlayer(3, 3)).toBe(2);
+});
