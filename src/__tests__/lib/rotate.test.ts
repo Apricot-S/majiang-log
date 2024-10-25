5796fa6543c371a86a470b697b0a998073872d75
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-25 02:08:06 +0000

    test: Add test cases

--- src/__tests__/lib/rotate.test.ts
+++ src/__tests__/lib/rotate.test.ts
@@ -33,18 +33,54 @@ test('player: 3, jushu: 3 -> position at the start of the game: 2', () => {
   expect(rotatePlayer(3, 3)).toBe(2);
 });
 
-test('[0, 1, 2, 3], jushu: 0 -> [0, 1, 2, 3]', () => {
+test('player: 0, jushu: -1 -> position in round: 3', () => {
+  expect(rotatePlayer(0, -1)).toBe(3);
+});
+
+test('player: 3, jushu: -1 -> position in round: 2', () => {
+  expect(rotatePlayer(3, -1)).toBe(2);
+});
+
+test('player: 0, jushu: -2 -> position in round: 2', () => {
+  expect(rotatePlayer(0, -2)).toBe(2);
+});
+
+test('player: 3, jushu: -2 -> position in round: 1', () => {
+  expect(rotatePlayer(3, -2)).toBe(1);
+});
+
+test('player: 0, jushu: -3 -> position in round: 1', () => {
+  expect(rotatePlayer(0, -3)).toBe(1);
+});
+
+test('player: 3, jushu: -3 -> position in round: 0', () => {
+  expect(rotatePlayer(3, -3)).toBe(0);
+});
+
+test('[0, 1, 2, 3], jushu: 0 -> order at the start of the game: [0, 1, 2, 3]', () => {
   expect(rotateOrder([0, 1, 2, 3], 0)).toEqual([0, 1, 2, 3]);
 });
 
-test('[0, 1, 2, 3], jushu: 1 -> [3, 0, 1, 2]', () => {
+test('[0, 1, 2, 3], jushu: 1 -> order at the start of the game: [3, 0, 1, 2]', () => {
   expect(rotateOrder([0, 1, 2, 3], 1)).toEqual([3, 0, 1, 2]);
 });
 
-test('[0, 1, 2, 3], jushu: 2 -> [2, 3, 0, 1]', () => {
+test('[0, 1, 2, 3], jushu: 2 -> order at the start of the game: [2, 3, 0, 1]', () => {
   expect(rotateOrder([0, 1, 2, 3], 2)).toEqual([2, 3, 0, 1]);
 });
 
-test('[0, 1, 2, 3], jushu: 3 -> [1, 2, 3, 0]', () => {
+test('[0, 1, 2, 3], jushu: 3 -> order at the start of the game: [1, 2, 3, 0]', () => {
   expect(rotateOrder([0, 1, 2, 3], 3)).toEqual([1, 2, 3, 0]);
+});
+
+test('[0, 1, 2, 3], jushu: -1 -> order in round: [1, 2, 3, 0]', () => {
+  expect(rotateOrder([0, 1, 2, 3], -1)).toEqual([1, 2, 3, 0]);
+});
+
+test('[0, 1, 2, 3], jushu: -2 -> order in round: [2, 3, 0, 1]', () => {
+  expect(rotateOrder([0, 1, 2, 3], -2)).toEqual([2, 3, 0, 1]);
+});
+
+test('[0, 1, 2, 3], jushu: -3 -> order in round: [3, 0, 1, 2]', () => {
+  expect(rotateOrder([0, 1, 2, 3], -3)).toEqual([3, 0, 1, 2]);
 });
