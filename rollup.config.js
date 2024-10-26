3c634effadbb4611f16a55e5c59fe4df1949974e
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-26 17:11:47 +0000

    chore: Add `rollup.config.js`

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
+
+import { readFile } from 'node:fs/promises';
+const pkg = JSON.parse(await readFile('./package.json'));
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: './dist/index.js',
+      format: 'es',
+      sourcemap: 'inline',
+    },
+    external: [
+      ...Object.keys(pkg.dependencies),
+      ...Object.keys(pkg.devDependencies),
+    ],
+    plugins: [typescript()],
+  },
+  {
+    input: 'src/command.ts',
+    output: {
+      file: 'dist/command.js',
+      format: 'es',
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({ declaration: false }),
+      terser(),
+      json(),
+    ],
+  },
+  {
+    input: 'src/server.ts',
+    output: {
+      file: 'dist/server.js',
+      format: 'es',
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({ declaration: false }),
+      terser(),
+      json(),
+    ],
+  },
+];
