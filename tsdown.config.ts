70a2d135cd8c5ee9ddbf8c035d6f3b0c146020d7
Apricot-S <mahjong_apricot@outlook.jp>
2026-02-20 20:08:45 +0000

    chore: Fix the extension to `js`

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,17 +3,20 @@ import { defineConfig } from 'tsdown';
 export default defineConfig([
   {
     entry: './src/index.ts',
+    fixedExtension: false,
     dts: true,
     sourcemap: true,
   },
   {
     entry: './src/command.ts',
+    fixedExtension: false,
     dts: false,
     minify: true,
     noExternal: ['ajv'],
   },
   {
     entry: './src/server.ts',
+    fixedExtension: false,
     dts: false,
     minify: true,
     noExternal: ['ajv', 'fastify'],
